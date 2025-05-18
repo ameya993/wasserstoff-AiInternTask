bc948f3baae7d9f6e1ed19d2e8339695a69887e1
ameya993 <ameya@example.com>
2025-05-18 17:02:59 +0530

    Update: made changes to files

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2025 Ameya Gawande. All rights reserved.
+# 
+# This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
+#
+# You may not use this file except in compliance with the License.
+# Non-commercial use only. Commercial use is strictly prohibited without written permission.
+
+
+
+
+
+
 from fastapi import APIRouter, UploadFile, File, HTTPException, Query
 from app.services.document_loader import load_documents
 from app.services.text_splitter import split_documents
--- backend/app/services/embedding.py
+++ backend/app/services/embedding.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2025 Ameya Gawande. All rights reserved.
+# 
+# This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
+#
+# You may not use this file except in compliance with the License.
+# Non-commercial use only. Commercial use is strictly prohibited without written permission.
+
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 import os
 
